dbdc9da6e72e03a2441170b73910822c4a739e02
Mesh <mesh@kali.kali>
2020-03-24 21:36:30 -0400

    added templates

--- myBlog/blog/models.py
+++ myBlog/blog/models.py
@@ -9,9 +9,9 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length = 200, unique=True)
     slug = models.SlugField(max_length=200,unique=True)
-    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+    content = models.TextField(null=False,blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS,default=0)
 
@@ -20,4 +20,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-        
+
